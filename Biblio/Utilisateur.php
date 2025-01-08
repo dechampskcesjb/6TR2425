20ce7803cd53f637371aeee4d1652c622b6acaaa
dechampskcesjb <kyllian.dechamps@cesjb.be>
2025-01-08 13:05:41 +0100

    Creatition de la classe Utilisateur

---
+++ Biblio/Utilisateur.php
@@ -0,0 +1,70 @@
+<?php
+
+class Utilisateur
+{
+
+    //Attributs
+    private string $nom;
+    private int $id;
+    private array $emprunts = [] ;
+
+
+    //Methodes GET ET SET
+
+
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getEmprunts(): string
+    {
+        return $this->emprunts;
+    }
+
+    public function setEmprunts(string $emprunts): void
+    {
+        $this->emprunts = $emprunts;
+    }
+
+    //Constructeur
+
+    /**
+     * @param string $nom
+     * @param int $id
+     */
+    public function __construct(string $nom, int $id)
+    {
+        $this->nom = $nom;
+        $this->id = $id;
+
+    }
+    //Methodes
+    public function emprunterLivre(Livre $livre)
+    {
+        if(!$livre->isEstEmprunte() ){
+            $livre->emprunter();
+            $this->emprunts[] = $livre;
+        }
+    }
+
+    public function retournerLivre(Livre $livre)
+    {
+    }
+}
